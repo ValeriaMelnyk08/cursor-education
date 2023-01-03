c8b565f1922f583f6ab6e7874131fbae8c0fa299
Valeria <valeriamelnik0@gmail.com>
2023-01-03 11:42:10 +0200

    add new HomeWork6

--- HW6/script.js
+++ HW6/script.js
@@ -31,43 +31,84 @@ const students = [{
 
     //========================================Перша функція===============================================================================================================================
   
-
-     function getSubjects (students) {
-
-     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //========================================================================================================================================
-    // function getAverageMark(students){
-      // let averageGrade = students.reduce((a, b) => a + b) / students.length;
-
-       //return averageGrade;
-     //}
-
-     //console.log(getAverageMark(students[0].subjects));
-
-     //===========================================================================================================================
-
-  function  getStudentsNames(students) { 
-    let  makeAlphabet = students.split('').sort().join('');
-    if 
+    function getSubjects (student) {
+      let subjects = Object.keys(student.subjects);//повертає масив власних перерахованих імен
+      let result = []; //нам потрібно вивести результат
+      for (let word of subjects) { //Отримує значення з послідовності на кожній ітерації
+        word = word.trim();// видаляє пробіли і повертає масив
+      
+        word = word.charAt(0).toUpperCase() + word.slice(1)// charAt повертає новий рядок із однією одиницею коду //slice() повертає поверхневу копію частини масиву в новий об’єкт масиву.
+      result.push(word);// додає кілька елементів в масив і повертає нову довжину
+    }
+    return result;
+  }
+ console.log(getSubjects(students[0]));
+     
+//==============================================Друга функція===================================================================================================================================================================
+ 
+function getAverageMark (student) {
+  let marks = Object.entries(student.subjects);
+  let total = 0;
+  let count = 0;
+  for (m of marks){
+       let sum = m[1].forEach(function(item, index) {//forEach()виконує надану функцію один раз для кожного елемента масиву.
+    total += item;
+    count++;
+});
+  }
+result = total / count;
+return result.toFixed(2);
 
 }
+console.log(getAverageMark(students[0]));
 
-console.log(makeAlphabet(students[0].name));
 
-    
+//=============================================Третя функція=======================================================================================================================================================================
+
+function getStudentInfo(student) {
+  let infoStudent = {};
+  infoStudent["Курс"] = student.course 
+  infoStudent["Ім'я"] = student.name
+  infoStudent["Середня оцінка"] = getAverageMark(student);
+  return infoStudent ;
+}
+
+
+console.log(getStudentInfo(students[0]));
+//==============================================Четверта функція===========================================================================
+function getStudentsNames(studentsNames) {
+  let result = [];
+  for (value of studentsNames) {
+    result.push(value.name);
+  }
+  return result.sort();
+}
+
+console.log(getStudentsNames(students));
+//=================================================П'ята функція  =================================================================================================================
+function getBestStudent(superStudent ) {
+  let bestGrade = students[0];
   
+  for (value of superStudent) {
+    if (getAverageMark(value) > getAverageMark(bestGrade) ) {
+      bestGrade = value;
+    }
+  }
+  return bestGrade.name
+} 
+
+console.log(getBestStudent(students));
+//===========================================Шоста функція ===================================================================================================================
+
+function calculateWordLetters(string) {
+  let divide = string.split("");
+  const count = {};
+
+  divide.forEach((word) => {
+      count[word] = count[word] ? (count[word] + 1) : 1;
+  });
+  return count;
+}
+console.log(calculateWordLetters("test"));
+
+
