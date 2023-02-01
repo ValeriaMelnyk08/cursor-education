18edf3ca247c5fe7b1dd57b5f806417140dc5c6d
Valeria <valeriamelnik0@gmail.com>
2023-02-01 10:33:34 +0200

    add new HW 13

---
+++ HW13/script.js
@@ -0,0 +1,63 @@
+//=======================Завдання 1 ============================================================================================
+function* createIdGenerator() {
+    for (let i = 0; i < Infinity; i++) {
+        yield i;
+        
+    }
+};
+
+const idGenerator = createIdGenerator();
+console.log( idGenerator.next().value);
+console.log( idGenerator.next().value); 
+console.log( idGenerator.next().value); 
+
+//=============================Завдання 2=================================================================================================================
+let newElement = document.createElement("p");
+document.body.append(newElement);
+newElement.innerText = 'Гарного дня';
+newElement.classList.add("newElement");
+newElement.style.textAlign = "center";
+newElement.style.color = "white";
+
+const newFontGenerator = {
+    generator(n) {
+        let i = n; 
+        return {
+            next (move) {
+                if (move === 'increase') {
+                    return { value: i += 2};
+                }
+
+                if (move === 'decrease') {
+                    return {value: i -= 2};
+                }
+            }
+        }
+    }
+}
+
+let fontSize = "";
+const iterator = newFontGenerator.generator(14);
+
+
+const btnIncrease = document.getElementById('increase');
+btnIncrease.addEventListener('click', () => {
+    fontSize = 'increase';
+    const size = iterator.next(`${fontSize}`).value;
+    newElement.style.fontSize = size + 'px';
+    console.log(size);
+})
+
+
+const btnDecrease = document.getElementById('decrease');
+btnDecrease.addEventListener('click', () => {
+    fontSize = 'decrease';
+    const size = iterator.next(`${fontSize}`).value;
+    newElement.style.fontSize = size + 'px';
+    console.log(size);
+})
+
+
+
+
+  
